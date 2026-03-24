c8832e47a8042cb30ed19568e04423fa6bc19675
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-03-25 06:29:49 +0700

    Create settings.ts

---
+++ src/data/settings.ts
@@ -0,0 +1,21 @@
+import type { SiteSettings } from '@/types';
+
+const DEFAULT_SETTINGS: SiteSettings = {
+  site_name: "AnimeXStream",
+  site_tagline: "Nonton Anime Subtitle Indonesia",
+  gojek_number: "6285000000001",
+  dana_link: "https://link.dana.id/qr/contoh",
+  ovo_link: "https://ovo.id/send/contoh",
+  shopee_link: "https://shopee.co.id/universal-link/contoh",
+  whatsapp_number: "6285000000001",
+  whatsapp_msg: "Halo, saya ingin donasi untuk support AnimeXStream 🙏",
+  qris_image: "",
+  admin_password: "admin123",
+  featured_slug: "naruto",
+  github_token: "",
+  github_owner: "",
+  github_repo: "",
+  github_branch: "main",
+};
+
+export default DEFAULT_SETTINGS;
