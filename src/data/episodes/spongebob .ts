fd3aab0a85bdf6101edcd4b2d1791181451ea41d
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-03-25 08:51:12 +0700

    chore: update episodes spongebob  [25/3/2026, 08.51.08]

---
+++ src/data/episodes/spongebob .ts
@@ -0,0 +1,17 @@
+import type { Episode } from '@/types';
+
+const episodes: Episode[] = [
+  {
+    "title": "Spongebob first a",
+    "description": "Lorem ipsum dolor sit amer",
+    "servers": [
+      {
+        "name": "Facebook",
+        "url": "https://www.facebook.com/share/v/1BENh4ngDP/"
+      }
+    ],
+    "number": 1
+  }
+];
+
+export default episodes;
