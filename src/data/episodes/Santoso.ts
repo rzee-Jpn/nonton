07a95202e5d80ee2e57513e981af9811402dd4e1
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-03-27 02:10:02 +0700

    chore: update episodes Santoso [27/3/2026, 02.09.59]

---
+++ src/data/episodes/Santoso.ts
@@ -0,0 +1,17 @@
+import type { Episode } from '@/types';
+
+const episodes: Episode[] = [
+  {
+    "title": "Berhenti jadi bucin",
+    "description": "Episode 1",
+    "servers": [
+      {
+        "name": "Bilibili",
+        "url": "https://player.bilibili.com/player.html?bvid=4798740364269056"
+      }
+    ],
+    "number": 1
+  }
+];
+
+export default episodes;
