8b0a54dd85cf176c9842a8a889ca9dfa3ad360c8
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-03-26 20:15:33 +0700

    chore: update episodes shinchan [26/3/2026, 20.15.31]

---
+++ src/data/episodes/shinchan.ts
@@ -0,0 +1,17 @@
+import type { Episode } from '@/types';
+
+const episodes: Episode[] = [
+  {
+    "title": "Tes",
+    "description": "Shinchan",
+    "servers": [
+      {
+        "name": "Youtube",
+        "url": "BajJ"
+      }
+    ],
+    "number": 1
+  }
+];
+
+export default episodes;
