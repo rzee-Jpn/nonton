230a530abc337a3032d867d93f07c898b2a22283
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-03-27 05:32:42 +0700

    chore: update episodes doraemon [27/3/2026, 05.32.40]

---
+++ src/data/episodes/doraemon.ts
@@ -0,0 +1,17 @@
+import type { Episode } from '@/types';
+
+const episodes: Episode[] = [
+  {
+    "title": "Tes",
+    "description": "Not tes",
+    "servers": [
+      {
+        "name": "Toku",
+        "url": "Rakuen"
+      }
+    ],
+    "number": 1
+  }
+];
+
+export default episodes;
