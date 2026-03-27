e66e42951eb399e715c5bd9c8e8425dcb4271865
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-03-27 15:02:43 +0700

    Update jjk.ts

--- src/data/episodes/jjk.ts
+++ src/data/episodes/jjk.ts
@@ -1,8 +1,42 @@
 import type { Episode } from '@/types';
 
 const episodes: Episode[] = [
-  { number: 1, title: "Ryomen Sukuna", description: "Yuji Itadori menelan jari Ryomen Sukuna untuk melindungi teman-temannya dari kutukan.", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/dQw4w9WgXcQ" }, { name: "VK", url: "https://vk.com/video_ext.php?oid=-22500&id=456245943&hash=b3ae4b4143e3e0be" }] },
-  { number: 2, title: "For Myself", description: "Yuji dibawa ke Tokyo Jujutsu High. Gojo Satoru memutuskan untuk memanfaatkan Yuji alih-alih mengeksekusinya.", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/dQw4w9WgXcQ" }] },
+  { number: 1, title: "《咒術迴戰》#1 (繁中字幕 | 日語原聲)【Ani-One Asia】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/miiLadou1cw" }] },
+  { number: 2, title: "《咒術迴戰》#2 (繁中字幕 | 日語原聲)【Ani-One Asia】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/YKnowHCkAA8" }] },
+
+  { number: 3, title: "《咒術迴戰》#3 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/v_vqYyQ5L9s" }] },
+  { number: 4, title: "《咒術迴戰》#4 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/K_AAlxO5Wrs" }] },
+
+  { number: 5, title: "《咒術迴戰》#5 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/sv0116pbo7w" }] },
+  { number: 6, title: "《咒術迴戰》#6 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/AH4ALxQH6As" }] },
+
+  { number: 7, title: "《咒術迴戰》#7 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/AGUMfvvaUis" }] },
+  { number: 8, title: "《咒術迴戰》#8 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/tZnDhFFwwVw" }] },
+
+  { number: 9, title: "《咒術迴戰》#9 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/GYUBvXZlqx4" }] },
+  { number: 10, title: "《咒術迴戰》#10 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/sMZdsoiNETk" }] },
+
+  { number: 11, title: "《咒術迴戰》#11 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/4wNf-kpJzvo" }] },
+  { number: 12, title: "《咒術迴戰》#12 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/GszgDgrBmTo" }] },
+
+  { number: 13, title: "《咒術迴戰》#13 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/jwUoJlNuoe4" }] },
+  { number: 14, title: "《咒術迴戰》#14 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/TEnMtNRcLe8" }] },
+
+  { number: 15, title: "《咒術迴戰》#15 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/wWtGRVymles" }] },
+  { number: 16, title: "《咒術迴戰》#16 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/S4L0i94txrw" }] },
+
+  { number: 17, title: "《咒術迴戰》#17 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/psDkK4nFIRk" }] },
+  { number: 18, title: "《咒術迴戰》#18 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/bNzNeQe76Fw" }] },
+
+  { number: 19, title: "《咒術迴戰》#19 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/J5JrmeHetPI" }] },
+  { number: 20, title: "《咒術迴戰》#20 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/7wowUlm-uss" }] },
+
+  { number: 21, title: "《咒術迴戰》#21 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/Vs8uPIv9BMg" }] },
+  { number: 22, title: "《咒術迴戰》#22 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/uIAT5XNDRIU" }] },
+
+  { number: 23, title: "《咒術迴戰》#23 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/a1kikdMkjlQ" }] },
+  { number: 24, title: "《咒術迴戰》#24 (繁中字幕 | 日語原聲)【Ani-One Asia ULTRA】", description: "💥《咒術迴戰》(第一季) 登陸Ani-One Asia ULTRA！", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/XAlpgO3Ijkg" }] },
+
 ];
 
 export default episodes;
