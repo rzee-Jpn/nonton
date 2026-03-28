eeabfdd3c9f610ee87c784e2c11529a5e382edc4
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-03-28 09:02:58 +0700

    Update elaina.ts

--- src/data/episodes/elaina.ts
+++ src/data/episodes/elaina.ts
@@ -1,17 +1,25 @@
 import type { Episode } from '@/types';
 
 const episodes: Episode[] = [
-  {
-    "title": "Jkklsosl",
-    "description": "Jsjsjs",
-    "servers": [
-      {
-        "name": "YouTube ",
-        "url": "Yt.com"
-      }
-    ],
-    "number": 1
-  }
+  { number: 1, title: "Wandering Witch: The Journey of Elaina - Episode 01 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: ", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/1rayl7gzKhU" }] },
+  { number: 2, title: "Wandering Witch: The Journey of Elaina - Episode 02 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: ", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/LCr_EnJOZig" }] },
+
+  { number: 3, title: "Wandering Witch: The Journey of Elaina - Episode 03 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: ", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/FT6UMRSCEvE" }] },
+  { number: 4, title: "Wandering Witch: The Journey of Elaina - Episode 04 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: ", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/6qA38Rb2EvE" }] },
+
+  { number: 5, title: "Wandering Witch: The Journey of Elaina - Episode 05 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: ", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/oR5Q8XiCNao" }] },
+  { number: 6, title: "Wandering Witch: The Journey of Elaina - Episode 06 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: ", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/ZTLtX4GZ5Fk" }] },
+
+  { number: 7, title: "Wandering Witch: The Journey of Elaina - Episode 07 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: https://www.youtube.com/channel/UCxxnxya_32jcKj4yN1_kD7A?sub_confirmation=1", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/4DQ4EC8Ttfw" }] },
+  { number: 8, title: "Wandering Witch: The Journey of Elaina - Episode 08 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: https://www.youtube.com/channel/UCxxnxya_32jcKj4yN1_kD7A?sub_confirmation=1", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/iWfTTLQNCaY" }] },
+
+  { number: 9, title: "Wandering Witch: The Journey of Elaina - Episode 09 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: https://www.youtube.com/channel/UCxxnxya_32jcKj4yN1_kD7A?sub_confirmation=1", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/OCIATvDD614" }] },
+  { number: 10, title: "Wandering Witch: The Journey of Elaina - Episode 10 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: https://www.youtube.com/channel/UCxxnxya_32jcKj4yN1_kD7A?sub_confirmation=1 ", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/ewBKj25zces" }] },
+
+  { number: 11, title: "Wandering Witch: The Journey of Elaina - Episode 11 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: https://www.youtube.com/channel/UCxxnxya_32jcKj4yN1_kD7A?sub_confirmation=1 ", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/Hbo1Ej9wMwQ" }] },
+  { number: 12, title: "Wandering Witch: The Journey of Elaina - Episode 12 [Takarir Indonesia]", description: "✅ Subscribe Muse Indonesia: https://www.youtube.com/channel/UCxxnxya_32jcKj4yN1_kD7A?sub_confirmation=1 ", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/ShauGFS-58g" }] },
+
+  { number: 13, title: "[📺Semua Episode] Wandering Witch: The Journey of Elaina", description: "✅ Subscribe Muse Indonesia: https://www.youtube.com/c/MuseID?sub_confirmation=1", servers: [{ name: "YouTube", url: "https://www.youtube.com/embed/rxLxEXvQmc0" }] },
 ];
 
 export default episodes;
