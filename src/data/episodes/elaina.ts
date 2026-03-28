c78acb2ff7dda02cfc2a7adb83bfeb5aa150dbee
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-03-28 09:01:55 +0700

    chore: update episodes elaina [28/3/2026, 09.01.42]

---
+++ src/data/episodes/elaina.ts
@@ -0,0 +1,17 @@
+import type { Episode } from '@/types';
+
+const episodes: Episode[] = [
+  {
+    "title": "Jkklsosl",
+    "description": "Jsjsjs",
+    "servers": [
+      {
+        "name": "YouTube ",
+        "url": "Yt.com"
+      }
+    ],
+    "number": 1
+  }
+];
+
+export default episodes;
